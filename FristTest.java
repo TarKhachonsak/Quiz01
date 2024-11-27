4199f42928c2570ec731918fb39a54d6809e3ab9
TarKhachonsak <khachonsak117@gmail.com>
2024-11-27 15:29:02 +0700

    Submit3

--- FristTest.java
+++ FristTest.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +12,29 @@ public class FristTest {
         frist.lastWaterMeter = 0;
     }
 
+    
+    @Test
+    public void testCalculateResultBill() {
+        frist.currentWaterMeter = 100;
+        frist.lastWaterMeter = 50;
+        frist.CurrentElectMeter = 30;
+        frist.lastElectMeter = 20;
+
+        int actual = frist.calculateResultBill("Double Room");
+        assertEquals(2310, actual); 
+    }
+
+    @Test
+    public void testCalculateResultBill2() {
+        frist.currentWaterMeter = 100;
+        frist.lastWaterMeter = 50;
+        frist.CurrentElectMeter = 30;
+        frist.lastElectMeter = 20;
+
+        int actual = frist.calculateResultBill("Sigle Room");
+        assertEquals(1810, actual); 
+    }
+
     @Test
     public void testCalculateWaterBill() {
         // กำหนดค่าเริ่มต้น
@@ -22,12 +46,12 @@ public class FristTest {
         assertEquals(250, actual); // (100 - 50) * 5 = 250
     }
 
-    @Test 
+    @Test
     public void testCalculateElectBill() {
         frist.CurrentElectMeter = 50;
         frist.lastElectMeter = 20;
 
         int actual = frist.calculateElectBill();
-        assertEquals(-120, actual); // (50 - 20) * 10 = 100
+        assertEquals(100, actual); // (50 - 20) * 10 = 100
     }
 }
--- frist.java
+++ frist.java
@@ -5,37 +5,37 @@ public class frist {
     public static int currentWaterMeter, lastWaterMeter;
     public static int CurrentElectMeter, lastElectMeter;
 
-    public static int calculateWaterBill() {
+    public static int calculateWaterBill() { //1
         if (currentWaterMeter >= lastWaterMeter) {
             waterM = (currentWaterMeter - lastWaterMeter) * 5;
         }
         return waterM;
     }
 
-    public static int calculateElectBill(){
+    public static int calculateElectBill(){ //2
         if (CurrentElectMeter >= lastElectMeter) {
-            ElectM = (currentWaterMeter - lastElectMeter) * 6;
+            ElectM = (CurrentElectMeter - lastElectMeter) * 6;
         }
         return ElectM;
     }
 
-    public static int calculateResultBill(String roomType){
+    public static int calculateResultBill(String roomType){ //3
         if (roomType.equals("Sigle Room")) {
             resultBill = 1500 + calculateWaterBill() + calculateElectBill();
-        } else if (roomType.equals("Double Room")){
+        } else if (roomType.equals("Double Room")){ //4
             resultBill = 2000 + calculateWaterBill() + calculateElectBill();
         }
         return resultBill;
     }
 
     public static void main(String[] args) {
-        CurrentElectMeter = 50;
+        CurrentElectMeter = 30;
         lastElectMeter = 20;
 
         currentWaterMeter = 100;
         lastWaterMeter = 50;
 
-        System.out.println("Your bill is: " + calculateResultBill("Double Room"));
+        System.out.println("Your bill is: " + calculateResultBill("Sigle Room")); //6
     }
     
 }
